7a78a96d33986b4b1710a920a408fead6c5e0e3e
nameofname <nameofemail@gmail.com>
2013-12-28 13:07:49 -0500

    Added letter spacing to the text on the home page.

--- page-home.php
+++ page-home.php
@@ -4,11 +4,27 @@
 
     <div class="row">
 
-        <div class="col-md-8 col-sm-8 col-xs-12">
-            <p id="home-text">
-                WEBSITE OF AUTHOR <span class="magenta">DANIELLE ROLLINS</span>.
-            </p>
+        <div class="col-md-2 col-sm-2 col-xs-12"><!-- This div provided to fill space to the left. --></div>
+
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <div id="home-text">
+                <div class="line-1">WEBSITE </div>
+                <div class="line-2">OF AUTH </div>
+                <div class="line-3">OR <span class="magenta">DANIE </span></div>
+                <div class="line-4"><span class="magenta">LLE ROLL </span></div>
+                <div class="line-5"><span class="magenta">INS</span>.</div>
+            </div>
         </div>
+
+<!--        <div class="col-md-5 col-sm-5 col-xs-12">-->
+<!--            <p id="home-text">-->
+<!--                WEBSITE <br />-->
+<!--                OF AUTH <br />-->
+<!--                OR <span class="magenta">DANI <br />-->
+<!--                ELLE RO <br />-->
+<!--                LLINS</span>.-->
+<!--            </p>-->
+<!--        </div>-->
 
         <div class="col-md-4 col-sm-4 col-xs-12 sidebar-right">
             <?php get_sidebar(); ?>
