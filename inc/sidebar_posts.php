0ce8751662a5e7f9b5894b1a170dba77f3c3e52e
nameofname <nameofemail@gmail.com>
2014-01-09 01:45:14 -0500

    Added some style to the sidebar list.

--- inc/sidebar_posts.php
+++ inc/sidebar_posts.php
@@ -1,9 +1,12 @@
 <?php
 
+// Get all posts tagged with "put_in_sidebar" -- then print a link for each:
 $sidebar_posts = get_sidebar_posts();
+$str = "<h2>A special title that Danielle made up.</h2><ul id='sidebar_posts'>";
 foreach ($sidebar_posts as $post) {
     $title = $post->post_title;
     $id = $post->ID;
-    echo "<a href='/?p=$id'>$title</a>";
+    $str .= "<li><a href='/?p=$id'>$title</a></li>";
 }
-
+$str .= "<ul>";
+echo $str;
