175399384161286c8af8915877328bb4b471322f
nameofname <nameofemail@gmail.com>
2014-01-18 18:07:44 -0500

    Added styling to the sidebar - and new sidebar posts section.

--- inc/sidebar_posts.php
+++ inc/sidebar_posts.php
@@ -1,12 +1,20 @@
+<br />
+<br />
+<p class="lead">A Special Title that Danielle made up</p>
+<ul id='sidebar_posts'>
 <?php
 
 // Get all posts tagged with "put_in_sidebar" -- then print a link for each:
 $sidebar_posts = get_sidebar_posts();
-$str = "<h2>A special title that Danielle made up.</h2><ul id='sidebar_posts'>";
+//$str = "<h3><i>A special title that Danielle made up.</i></h3><ul id='sidebar_posts'>";
+$str = "";
 foreach ($sidebar_posts as $post) {
     $title = $post->post_title;
     $id = $post->ID;
     $str .= "<li><a href='/?p=$id'>$title</a></li>";
 }
-$str .= "<ul>";
+//$str .= "<ul>";
 echo $str;
+?>
+
+</ul>
--- sidebar.php
+++ sidebar.php
@@ -1,25 +1,29 @@
-<ul id="sidebar">
+<div id="sidebar">
+
+    <ul id="sidebar-nav">
+        <?php
+
+        // Page links:
+        echo wp_list_pages('title_li=&depth=1');
+
+        ?>
+    </ul>
+
     <?php
 
-    // Page links:
-    echo wp_list_pages('title_li=&depth=1');
+    // If this is not the home page, then add the last tweet:
+    if (!is_page('home')) {
+
+        include_once(THEME_ROOT . '/inc/last_tweet.php');
+
+        echo '<br />';
+
+        include(THEME_ROOT . '/inc/twitter_follow.php');
+
+        include(THEME_ROOT . '/inc/sidebar_posts.php');
+
+    }
 
     ?>
-</ul>
 
-<?php
-
-// If this is not the home page, then add the last tweet:
-if (!is_page('home')) {
-
-    include_once(THEME_ROOT . '/inc/last_tweet.php');
-
-    echo '<br />';
-
-    include(THEME_ROOT . '/inc/twitter_follow.php');
-
-    include(THEME_ROOT . '/inc/sidebar_posts.php');
-
-}
-
-?>
+</div>
