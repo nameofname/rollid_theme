f6219bfecbf983b92eb0f56ac7217d9659d4fafe
nameofname <nameofemail@gmail.com>
2014-02-18 00:26:37 -0500

    Removed twitter followers number.

--- inc/twitter_follow.php
+++ inc/twitter_follow.php
@@ -1,1 +1,1 @@
-<a href="https://twitter.com/dvegabooks" class="twitter-follow-button" data-show-count="true" data-lang="en">Follow @dvegabooks</a>
+<a href="https://twitter.com/dvegabooks" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @dvegabooks</a>
