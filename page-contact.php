f691e15d90dd0dcb56b0ef27aad924279456c982
nameofname <nameofemail@gmail.com>
2014-03-03 23:39:18 -0500

    Updated shortcode on contact form page to the one generated by prod admin screen. Also added html file with markup to paste into the contact form.

--- page-contact.php
+++ page-contact.php
@@ -14,13 +14,7 @@
                 (<span class="magenta "><i class="fa fa-female"></i> <i class="fa fa-plus"></i> <i class="fa fa-envelope"></i></span>)
             </h1>
             <?php
-
-            echo do_shortcode( '[contact-form-7 id="1429" title="Contact form 1"]' );
-
-                // Note: No content will be displayed on this page:
-    //            $page_data = get_page($page_id);
-    //            echo $page_data->post_content;
-
+            echo do_shortcode( '[contact-form-7 id="1419" title="Contact form 1"]' );
             ?>
 
         </div>
